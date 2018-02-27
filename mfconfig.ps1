4aee3cdf66bc390d132b960a9d30a5a922e3253f
Łukasz Kołosiński <kolosinski_l@solaris_pl.solaris.net>
2018-02-27 10:20:10 +0100

    Intitial Commit

---
+++ mfconfig.ps1
@@ -0,0 +1,22 @@
+﻿###### SKRYPT AKTYWUJACY IBM NOTES ORAZ IMPORTUJACY ZAKLADKI DO FIREFOXA
+
+function copyFirefoxFiles($profile_full_path) {
+    Copy-Item ".\Profilefiles\places.sqlite" -Destination $profile_full_path -Force
+    Copy-Item ".\Profilefiles\xulstore.json" -Destination $profile_full_path -Force
+    Copy-Item ".\Profilefiles\extensions.json" -Destination $profile_full_path -Force
+    Copy-Item ".\Profilefiles\extensions.ini" -Destination $profile_full_path -Force
+}
+
+if(Test-Path D:\Users\$env:UserName\AppData\Roaming\Mozilla){
+    Set-Location D:\Users\$env:UserName\AppData\Roaming\Mozilla\Firefox\Profiles
+    $profile_folder = Get-ChildItem -Filter *3.default | Select -exp Name
+    $profile_full_path = "D:\Users\kolosinski_l\AppData\Roaming\Mozilla\Firefox\Profiles" + $profile_folder
+    copyFirefoxFiles $profile_full_path
+} else {
+    Start-Process firefox.exe
+    Set-Location D:\Users\kolosinski_l\AppData\Roaming\Mozilla\Firefox\Profiles
+    $profile_folder = Get-ChildItem -Filter *3.default | Select -exp Name
+    $profile_full_path = "D:\Users\kolosinski_l\AppData\Roaming\Mozilla\Firefox\Profiles" + $profile_folder
+    copyFirefoxFiles $profile_full_path
+}
+    
